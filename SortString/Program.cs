7986cfd31a04c77a15b24a15aad7af583626a8bb
samufromhungary <blzkproblem@gmail.com>
2019-08-14 14:20:20 +0200

    Add project files.

---
+++ SortString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Microsoft .NET Framework 4.7 Application Development Foundation";
+            string[] words = str.Split(' ');
+            Array.Sort(words);
+            var s = string.Join(" ", words);
+            Console.WriteLine(s);
+            Console.ReadKey(true);
+        }
+    }
+}
